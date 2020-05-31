b41487a719cde16ab8137e0b3fe300207b8d781d
nisunisu <sitos.nishi@gmail.com>
2020-05-31 16:26:03 +0900

    :art: Add tags

--- aws/example.tf
+++ aws/example.tf
@@ -6,9 +6,17 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0a1c2ec61571737db" # amazon linux
   instance_type = "t2.micro"
+  tags = {
+    Name  = "terraform-test"
+    Owner = "nisunisu"
+  }
 }
 
 resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+  vpc      = true
+  instance = aws_instance.example.id
+  tags = {
+    Name  = "EIP_terraform_test"
+    Owner = "nisunisu"
+  }
 }
