6328a6e110c357d97e0282dcf0b4c35551e34eed
nisunisu <sitos.nishi@gmail.com>
2020-07-28 09:19:56 +0900

    :art: Update AMI id to the one which is created with Packer

--- tf_ec2_blue/ec2.tf
+++ tf_ec2_blue/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_1" {
-  ami           = "ami-0f494d7037b774834" # Personal AMI (Amazon linux v2 with nginx(enabled) and mysql client)
+  ami           = "ami-00b8ba5422dd106cd" # Use latest amazon linux 2 ami created with Packer
   instance_type = "t2.micro"
   vpc_security_group_ids = [
     aws_security_group.ec2_blue.id
