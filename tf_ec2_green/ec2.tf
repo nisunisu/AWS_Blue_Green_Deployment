ca70f5e8b33282a8a34646059260ab8a943b3eda
nisunisu <sitos.nishi@gmail.com>
2020-08-01 17:40:25 +0900

    :art: Add keypair create info to README.md and so replace EC2 keypair name (excluding bastion EC2)

--- tf_ec2_green/ec2.tf
+++ tf_ec2_green/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web_green" {
   ]
   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id_private_1c
   associate_public_ip_address = false # NOTICE: Even if this is set as "false", it will be ALWAYS set as "true" when "auto-assign public ipv4 address" with SUBNET is set as "TRUE".
-  key_name                    = var.key_name
+  key_name                    = "keypair_terraform" # See readme.md which describes how to create this keypair
   tags = {
     Name  = "ec2_web_green_terraform"
   }
--- tf_ec2_green/variables.tf
+++
@@ -1,1 +0,0 @@
-variable "key_name" {}
